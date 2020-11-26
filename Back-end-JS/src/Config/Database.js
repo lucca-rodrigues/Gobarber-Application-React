1973ce0538452cae27596757d1f5644684fd5c1a
Lucca Rodrigues <contato.luccarodrigues@gmail.com>
2020-11-26 20:05:08 -0300

    add new conection database

--- Back-end-JS/src/Config/Database.js
+++ Back-end-JS/src/Config/Database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
+  host: '192.168.99.100',
   username: 'postgres',
-  password: 'docker',
-  database: 'gobarber',
+  password: 'gobarber',
+  database: 'postgres',
   define: {
     timestamps: true,
     underscored: true,
