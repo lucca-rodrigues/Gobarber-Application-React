29eefe1b99f721a270e309a3972e539c97cba427
Lucca Rodrigues <contato.luccarodrigues@gmail.com>
2020-12-01 19:39:28 -0300

    reset password page

--- Front-end/src/Routes/index.tsx
+++ Front-end/src/Routes/index.tsx
@@ -4,6 +4,7 @@ import Route from './Route';
 import SignIn from '../Pages/SignIn';
 import SignUp from '../Pages/SignUp';
 import Dashboard from '../Pages/Dashboard';
+import ForgotPassword from '../Pages/ForgotPassword';
 
 const Routes: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const Routes: React.FC = () => {
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" component={SignUp} />
       <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/forgot" component={ForgotPassword} />
     </Switch>
   )
 }
